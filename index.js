8e50abb1e03df9278b9314784bf433426f94e821
Aahan Sharma <sharmaaahan15@gmail.com>
2021-04-03 18:27:13 +0000

    print image data text

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var tesseract = require('tesseract.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    var textDat = "";
   tesseract.recognize(
     'https://tesseract.projectnaptha.com/img/eng_bw.png',
     'eng',
     { logger: m => console.log(m) }
     ).then(({ data: { text } }) => {
        console.log(text);
-       res.end(text);
+       textDat = text; 
+       res.write('image data \n');
+       res.write(textDat);
+        res.end();
   })
-    res.end('Hello World!');
+    res.write(textDat)
+    
+    
 }).listen(8080);
